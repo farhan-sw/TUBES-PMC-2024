5ecf8261c0a8f6813382d71e418e8c2d9a45b93b
Chobirudin Jamil Yudaprawira <119297958+Chobirudin@users.noreply.github.com>
2024-05-15 11:01:45 +0700

    Update greedy.c

--- Algoritma/greedy.c
+++ Algoritma/greedy.c
@@ -5,11 +5,8 @@
 #include <time.h>
 #include "openFile.h"
 
-#define pi 3.1416
-
 /* 
 Dibuat oleh : Chobirudin Jamil Yudaprawira (13222113)
-Dibuat pada : 11/05/2024
 Deskripsi   : Implementasi algoritma greedy untuk soal 2 tubes PPMC - EL2208
 Referensi   : 
 https://www.tutorialspoint.com/data_structures_algorithms/travelling_salesman_problem.htm
@@ -17,6 +14,7 @@ https://www.geeksforgeeks.org/traveling-salesman-problem-tsp-implementation/
 https://www.ibm.com/docs/en/zos/2.4.0?topic=functions-clock-determine-processor-time
 */
 
+// Fungsi untuk mencari indeks dalam matriks dari suatu kota
 void search(char namakota[255], char kota[15][255], int *ind){
     int i = 0;
     for(i = 0; i < 15; i++){
@@ -26,15 +24,15 @@ void search(char namakota[255], char kota[15][255], int *ind){
     }
 }
 
+// Fungsi untuk menyelesaikan Travelling Salesman Problem dengan algoritma greedy
 void travellingsalesman(int current_vertex, int starting_vertex, float matrix[15][15], int visited[10], int n, float *cost, char mat_kota[15][255]) {
    int k, adj_vertex = 999;
-   float min = 9999999999;
-   float unused1, unused2;
+   float min = 9999999999;                                          // Diasumsikan jarak antarkota tidak ada yang melebihi ini
    
-   visited[current_vertex] = 1;
-   printf("%s -> ", mat_kota[current_vertex]);
+   visited[current_vertex] = 1;                                     // Menandai kota yang telah dikunjungi
+   printf("%s -> ", mat_kota[current_vertex]);                      // Menampilkan kota yang sedang dikunjungi
    
-   for(k = 0; k < n; k++) {
+   for(k = 0; k < n; k++) {                                         // Memeriksa semua kota yang belum dikunjungi dan mencari jarak minimum
       if((current_vertex != k) && (visited[k] == 0)) {
          if(matrix[current_vertex][k] < min) {
             min = matrix[current_vertex][k];
@@ -43,18 +41,19 @@ void travellingsalesman(int current_vertex, int starting_vertex, float matrix[15
       }
    }
    
-   if(min != 999) {
+   if(min != 999) {                                                 // Jika ada jarak minimum cost/jarak ditambahkan
       (*cost) = (*cost) + min;
    }
    if(adj_vertex == 999) {
-      printf("%s", mat_kota[starting_vertex]);
-      (*cost) = (*cost) + matrix[current_vertex][starting_vertex];
+      printf("%s", mat_kota[starting_vertex]);                      // Mencetak kota awal jika semua kota sudah dikunjungi
+      (*cost) = (*cost) + matrix[current_vertex][starting_vertex];  // Mencetak jarak kota awal ke kota awal
       return;
    }
-   travellingsalesman(adj_vertex, starting_vertex, matrix, visited, n, cost, mat_kota);
+   travellingsalesman(adj_vertex, starting_vertex, matrix, visited, n, cost, mat_kota); // Rekursif
 }
 
 int main(){
+    // Inisialisasi
     clock_t start_time, end_time;
     double timedif;
     char filename[255], buff[255], namaKota[15][255];
@@ -70,30 +69,26 @@ int main(){
         visited[i] = 0;
     }
 
+    // Input nama file
     printf("Enter list of cities file name: ");
     scanf("%s", filename);
+
+    // Membuka file dan menyimpan jarak dalam matrix
     if(open_init(filename, matrix, namaKota, &n) == 0) {
         return 0;
     }
 
+    // Input kota awal
     printf("Enter starting point: ");
     scanf("%s", buff);
     search(buff, namaKota, &ind_kota);
-
-    // for(i = 0; i < n; i++) {
-    //     for(j = 0; j < n; j++) {
-    //         printf("%f ", matrix[i][j]);
-    //         }
-    //     printf("\n");
-    // }
-
     printf("Best route found: ");
-    start_time = clock();
-    travellingsalesman(ind_kota, ind_kota,matrix, visited, n, &cost, namaKota);
-    end_time = clock();
-    timedif = ((double) (end_time - start_time))/CLOCKS_PER_SEC;
-    printf("\nBest route distance: ");
+    start_time = clock();                                                       // Waktu mulai menjalankan algoritma
+    travellingsalesman(ind_kota, ind_kota,matrix, visited, n, &cost, namaKota); // Memanggil algoritma greedy
+    end_time = clock();                                                         // Waktu selesai menjalankan algoritma
+    timedif = ((double) (end_time - start_time))/CLOCKS_PER_SEC;                // Menghitung selisih waktu
+    printf("\nBest route distance: ");                                          // Mencetak output pada layar
     printf("%f\n", cost);
     printf("Time elapsed: %.20f s\n", timedif);
-    return 0;
+    return 0;                                                                   // Program berakhir
 }
